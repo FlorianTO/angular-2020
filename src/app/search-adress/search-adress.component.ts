7f5759f574d915e4392f18688f75256af1c05099
Florian Tourinel <floriantourinel@live.fr>
2020-11-29 21:49:36 +0100

    tests

--- src/app/search-adress/search-adress.component.ts
+++ src/app/search-adress/search-adress.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { MessageService } from 'app/message.service';
 
 @Component({
